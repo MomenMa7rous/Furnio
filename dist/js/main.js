6f93ce72cb2002e587ee9f39ea829cff9ffd2779
MomenMa7rous <momenma7rous@gmail.com>
2026-02-27 02:09:14 +0200

    Create rooms section.

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,3 +10,34 @@ document.addEventListener("click", (ev) => {
         overlay.classList.toggle("translate-x-full")
     }
 })
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+    autoplay: {
+        delay: 5000,
+    },
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    breakpoints: {
+        992: {
+            slidesPerView: 2,
+            // spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 3,
+            // spaceBetween: 40
+        }
+    }
+});
